c3b0b803eabecac0c2362772e31e80196e7366a1
Tadej Stanonik <ts6103@student.uni-lj.si>
2021-10-31 15:04:57 +0100

    Dodane XML datoteke

---
+++ models/src/main/resources/sql-scripts/init.sql
@@ -0,0 +1,1 @@
+INSERT INTO user (name, surname, google_id, email) VALUES ('Jože', 'Gorišek', 123456789, 'joze.gorisek@gmail.com');
--- services/src/main/java/si/fri/rso/skupina3/user/services/producers/PersistenceProducer.java
+++ services/src/main/java/si/fri/rso/skupina3/user/services/producers/PersistenceProducer.java
@@ -8,7 +8,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
 public class PersistenceProducer {
-    @PersistenceUnit(unitName = "image-catalog-jpa")
+    @PersistenceUnit(unitName = "user-jpa")
     private EntityManagerFactory emf;
 
     @Produces
