63e13db5e214fe43f2c971c7314d94a91e14bd78
Tadej Stanonik <ts6103@student.uni-lj.si>
2022-01-08 00:06:21 +0100

    Discovery

--- api/src/main/java/si/fri/rso/skupina3/user/v1/UserApplication.java
+++ api/src/main/java/si/fri/rso/skupina3/user/v1/UserApplication.java
@@ -6,6 +6,6 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
-@RegisterService(value="user-service", environment="dev", version = "1.0.0")
+//@RegisterService(value="user-service", environment="dev", version = "1.0.0")
 public class UserApplication extends Application {
 }
